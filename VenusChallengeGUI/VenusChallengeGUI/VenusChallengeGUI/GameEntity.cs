60134bc73885f8228f127f6f148078268ef365c9
Sachi <sachithradangalla@gmail.com>
2015-12-07 22:00:01 +0530

    Fixed classes

--- VenusChallengeGUI/VenusChallengeGUI/VenusChallengeGUI/GameEntity.cs
+++ VenusChallengeGUI/VenusChallengeGUI/VenusChallengeGUI/GameEntity.cs
@@ -1,20 +1,23 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyTankGame
+namespace VenusChallengeGUI
 {
     abstract class GameEntity
     {
         public int x; //horizontal position
         public int y; //vertical position
+        public Vector2 pos;
         public string playerName;
         public GameEntity(int xx, int yy)
         {
             x = xx;
             y = yy;
+            pos = new Vector2(x, y);
         }
         public GameEntity()
         {
