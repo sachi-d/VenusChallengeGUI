37e9d503818efaa42ebad2da2db4948c628039aa
Yasara <yasarapeiris.13@cse.mrt.ac.lk>
2015-12-13 14:19:14 +0530

    AI class and node classes added.

---
+++ VenusChallengeGUI/VenusChallengeGUI/VenusChallengeGUI/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VenusChallengeGUI
+{
+    class Node
+    {
+        bool terminalNode;
+
+        public List<Node> Children(bool Player)
+        {
+            List<Node> children = new List<Node>();
+
+            // Create your subtree here and return the results
+
+            return children;
+        }
+
+        public bool IsTerminal(bool Player)
+        {
+            terminalNode = false;
+
+            // Game over?
+
+            return terminalNode;
+        }
+
+        public int GetTotalScore(bool Player)
+        {
+            int totalScore = 0;
+
+            // This method is a heuristic evaluation function to evaluate
+            // the current situation of the player
+            // It depends on the game. For example chess, tic-tac-to or other games suitable
+            // for the minimax algorithm can have different evaluation functions.
+
+            return totalScore;
+        }
+    }
+}
